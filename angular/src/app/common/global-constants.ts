8c5a2e7bbbe1309ea731cc270db4134b030add00
jdec92 <juande_estrella@uma.es>
2020-11-18 17:19:45 +0100

    Added functionality displaying recipes FrontEnd

--- angular/src/app/common/global-constants.ts
+++ angular/src/app/common/global-constants.ts
@@ -1,3 +1,8 @@
-export class GlobalConstants{
-  public static apiUrl: string = "http://localhost/api";
+export class GlobalConstants {
+  public static apiUrl: string = 'http://localhost/api';
+  public static addRecipe: string = '/add';
+  public static editRecipe: string = '/edit';
+  public static showCategory: string = '/category';
+  public static showRecipe: string = '/show/';
+  public static showUltimateRecipe: string = '/ultimate/recipes/';
 }
--- angular/src/app/layouts/welcome/components/welcome.component.ts
+++ angular/src/app/layouts/welcome/components/welcome.component.ts
@@ -1,18 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { WelcomeService } from '../service/welcome.service';
-
-declare interface Recipe {
-  id: string;
-  img_path: string;
-  title: string;
-  description: string;
-}
-
-export const RECIPES: Recipe[] = [
-  { id:'1', img_path: './assets/img/recipes/001.jpg', title: 'Solomillo al Jerez',  description: '1.- Comenzamos poniendo salpimentando...'},
-  { id:'2', img_path: './assets/img/recipes/002.jpg', title: 'Brocoli al Curry',  description: '1.- Comenzamos poniendo salpimentando...'},
-  { id:'3', img_path: './assets/img/default.jpg', title: 'Salmón a la Naranja',  description: '1.- Comenzamos poniendo salpimentando...'},
-];
+import {Component, OnInit} from '@angular/core';
+import {WelcomeService} from '../service/welcome.service';
+import {Recipe} from '../../../models/recipe';
+import {GlobalConstants} from '../../../common/global-constants';
 
 @Component({
   selector: 'app-welcome',
@@ -20,19 +9,21 @@ export const RECIPES: Recipe[] = [
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-  bestRecipes: any[];
 
-  constructor(public service: WelcomeService) { }
+  bestRecipes: Recipe[];
+  urlAddRecipe: string = GlobalConstants.addRecipe;
+  urlRecipe: string = GlobalConstants.showRecipe;
+
+  constructor(public service: WelcomeService) {
+  }
 
   ngOnInit() {
-    this.service.recipesUltimate(3).subscribe(data => {
-      console.log(data);
-        this.bestRecipes = data;
+    this.service.recipesUltimate(3).subscribe((data: Recipe[]) => {
+             this.bestRecipes = data;
       },
       err => {
         console.log(err);
       });
-    this.bestRecipes = RECIPES.filter(bestRecipe => bestRecipe);
   }
 
 }
